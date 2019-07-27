7122067c1cd78fb4f6b5d832d467ed8889d0543d
mickael2407 <pezzoni.mickael@gmail.com>
2019-07-27 18:11:27 +0200

    add tab navigator

---
+++ Components/Favorites.js
@@ -0,0 +1,20 @@
+// Components/FilmItem.js
+
+import React from 'react'
+import { StyleSheet, Text } from 'react-native'
+
+
+class Favorites extends React.Component {
+
+    render() {
+        return (
+            <Text>Mes favoris</Text>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+})
+
+
+export default Favorites;
--- navigation/navigation.js
+++ navigation/navigation.js
@@ -1,6 +1,9 @@
-import { createAppContainer, createStackNavigator } from 'react-navigation';
+import React from 'react';
+import { Image, StyleSheet } from 'react-native';
+import { createAppContainer, createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 import { Search } from '../Components/Search';
 import FilmDetail from '../Components/FilmDetail';
+import Favorites from '../Components/Favorites';
 
 const SearchStackNavigator = createStackNavigator({
     Search: {
@@ -12,5 +15,42 @@ const SearchStackNavigator = createStackNavigator({
     FilmDetail: {
         screen: FilmDetail
     }
+});
+
+
+const MoviesTabNavigator = createBottomTabNavigator({
+    Search: {
+        screen: SearchStackNavigator,
+        navigationOptions: {
+            tabBarIcon: () => {
+                return <Image
+                    soource={require('../assets/ic_search.png')}
+                    style={styles.icon} />
+            }
+        }
+    },
+    Favorites: {
+        screen: Favorites,
+        navigationOptions: {
+            tabBarIcon: () => {
+                return <Image
+                    soource={require('../assets/icon.png')}
+                    style={styles.icon} />
+            }
+        }
+    }
+},
+    {
+        tabBarOptions: {
+            showLabel: false,
+            showIcon: true
+        }
+    });
+
+const styles = StyleSheet.create({
+    icon: {
+        width: 30,
+        height: 30
+    }
 })
-export default createAppContainer(SearchStackNavigator);
+export default createAppContainer(MoviesTabNavigator);
